3256d2bb8bd75f5904a4f4dfc2be7ebd020049ce
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-08-04 23:05:52 -0300

    feat(settings.py): configure settings file

--- api_root/settings.py
+++ api_root/settings.py
@@ -38,9 +38,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "corsheaders",
+    # "corsheaders",
+    "knox",
     "api_rest",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": ("knox.auth.TokenAuthentication",),
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
